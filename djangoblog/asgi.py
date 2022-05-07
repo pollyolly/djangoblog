ad3c93e61e27808ee9947928d2ec3e21d723de39
root <root@DESKTOP-DG1IPK2.localdomain>
2022-05-07 19:24:58 +0800

    Added allowed host origin security for websocket

--- djangoblog/asgi.py
+++ djangoblog/asgi.py
@@ -15,15 +15,19 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 
 from channels.auth import AuthMiddlewareStack
 
+from channels.security.websocket import AllowedHostsOriginValidator
+
 import chat.websocket_routing as routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoblog.settings')
 
 application = ProtocolTypeRouter({
         'http':get_asgi_application(),
-        'websocket':AuthMiddlewareStack(
-            URLRouter(
-                routing.websocket_urlpatterns
+        'websocket': AllowedHostsOriginValidator( #ALLOWED_HOSTS/Origin in settings.py SECURITY PURPOSES
+            AuthMiddlewareStack(
+                URLRouter(
+                    routing.websocket_urlpatterns
+                )
             )
         )
     })

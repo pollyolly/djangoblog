1c49a818c6e97cfccf3e5f501984979ceb36fb24
pollyolly <johnmarkroco05@gmail.com>
2023-01-15 11:06:45 +0000

    Fix 404 template location

--- djangoblog/views.py
+++ djangoblog/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 # temtplates/base/
-def handler404(request, exception=None, template_name='404.html'):
+def handler404(request, exception=None, template_name='base/404.html'):
     return render(request, template_name, {'status':'404'})
